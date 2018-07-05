00f84e63750c16aca07bc28e1f7fe62cbb0fcceb
Andrei Biasprozvanny <stereoderevo@gmail.com>
2018-07-05 08:43:46 +0100

    prettier

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
-import Toggle from './components/toggle/toggle'
+import React, { Component } from "react";
+import "./App.css";
+import Toggle from "./components/toggle/toggle";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="App-title"><span>Wisen switch</span></h1>
-          <Toggle onToggle={(...args) => console.log('Wisen is', ...args)} />
+        <h1 className="App-title">
+          <span>Wisen switch</span>
+        </h1>
+        <Toggle onToggle={(...args) => console.log("Wisen is", ...args)} />
       </div>
     );
   }
