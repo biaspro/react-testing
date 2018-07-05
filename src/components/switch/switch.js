4b9f905f8ec404b4db97b4892f2850d897e2258b
Andrei Biasprozvanny <stereoderevo@gmail.com>
2018-07-05 20:09:06 +0100

    more prettier

--- src/components/switch/switch.js
+++ src/components/switch/switch.js
@@ -1,38 +1,32 @@
-import './switch.styles.css'
-import React from 'react'
+import "./switch.styles.css";
+import React, { PropTypes } from "react";
 
-const noop = () => {}
+const noop = () => {};
 
 class Switch extends React.Component {
-    render() {
-        const {
-            on,
-            'aria-label': ariaLabel,
-            onClick,
-            ...props
-        } = this.props
-        const btnClassName = [
-            'toggle-btn',
-            on ? 'toggle-btn-on' : 'toggle-btn-off',
-        ]
-            .join(' ')
-        return (
-            <label
-                aria-label={ariaLabel || 'Toggle'}
-                style={{display: 'inline-block'}}
-            >
-                <input
-                    className="toggle-input"
-                    type="checkbox"
-                    checked={on}
-                    onChange={noop}
-                    onClick={onClick}
-                    data-testid="toggle-input"
-                />
-                <span className={btnClassName} {...props} />
-            </label>
-        )
-    }
+  render() {
+    const { on, "aria-label": ariaLabel, onClick, ...props } = this.props;
+    const btnClassName = [
+      "toggle-btn",
+      on ? "toggle-btn-on" : "toggle-btn-off"
+    ].join(" ");
+    return (
+      <label
+        aria-label={ariaLabel || "Toggle"}
+        style={{ display: "inline-block" }}
+      >
+        <input
+          className="toggle-input"
+          type="checkbox"
+          checked={on}
+          onChange={noop}
+          onClick={onClick}
+          data-testid="toggle-input"
+        />
+        <span className={btnClassName} {...props} />
+      </label>
+    );
+  }
 }
 
-export {Switch}
+export default Switch;
---
+++ src/components/switch/switch.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Switch from "./switch";
+
+test("hey", () => {
+  //arrange
+  const container = document.createElement("div");
+  ReactDOM.render(<Switch />, container);
+
+  //act
+
+  //assert
+});
